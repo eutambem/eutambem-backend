103c21bc299ac4b8f06825d9e22782371d352317
Rafaela Prado <rprado@thoughtworks.com>
2018-09-24 15:21:46 -0300

    add db connection

--- app.js
+++ app.js
@@ -7,4 +7,22 @@ var constantsRouter = express.Router();
 constantsRouter.get('/', (req, res) => res.json(constants));
 app.use('/constants', constantsRouter);
 
+
+app.get('/db', function(req, res) {
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host : 'eutambem-db-cluster.cluster-cta4f3s1nekd.us-east-1.rds.amazonaws.com',
+        user : 'admin',
+        password : 'zQ4hMn7GX3',
+        database : 'eutbm'
+    });
+     connection.query('select * from livros',function(err, results) {
+        if(err) {
+            console.log(err);
+        }
+        res.send({lista : results});
+     });
+     connection.end();
+    });
+
 module.exports = app;
