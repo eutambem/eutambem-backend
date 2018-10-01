59905bf6efb1e200beaf8e6673ab5ec14757d3bf
Rafaela Prado <rprado@thoughtworks.com>
2018-10-01 16:51:37 -0300

    add database env variable

--- app/db/connectionFactory.js
+++ app/db/connectionFactory.js
@@ -1,7 +1,6 @@
 'use strict';
 const MongoClient = require('mongodb').MongoClient;
-let _db;
-const uri = `mongodb://admin:H3]G8*LXcH66e&$hrLsA@eutambem-shard-00-00-qrwe2.mongodb.net:27017,eutambem-shard-00-01-qrwe2.mongodb.net:27017,eutambem-shard-00-02-qrwe2.mongodb.net:27017/test?replicaSet=eutambem-shard-0&ssl=true&authSource=admin`;
+const uri = process.env.MONGO_CONN;
 
 module.exports = {
     connectToServer: function(callback) {
