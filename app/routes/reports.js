70fafef639d3689951f7fba20f6678273fddcca2
Rafaela Prado <rprado@thoughtworks.com>
2018-10-01 17:29:56 -0300

    refactor database connection

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -6,25 +6,21 @@
      app.post('/report', function(req, res) { 
         const reportObj = req.body;
         let mongoUtil = app.db.connectionFactory;
-        mongoUtil.connectToServer(function(err, db) {
-            dbo = mongoUtil.getDb();
-            dbo.collection("report").insertOne(reportObj, function(err, resdb) {
-                if (err) throw err;
-                res.send('1 report inserted');
-                db.close();
-            });
+
+        dbo = mongoUtil.getDb();
+        dbo.collection("report").insertOne(reportObj, function(err, resdb) {
+            if (err) throw err;
+            res.send('1 report inserted');
         });
     });
     
     app.get('/report', function(req, res) {
         let mongoUtil = app.db.connectionFactory;
-        mongoUtil.connectToServer(function(err, db) {
-            dbo = mongoUtil.getDb();
-            dbo.collection("report").find({}).toArray(function(err, result) {
+
+        dbo = mongoUtil.getDb();
+        dbo.collection("report").find({}).toArray(function(err, result) {
             if (err) throw err;
             res.send({list : result});
-            db.close();
-            });
         });
     });
 }
--- config/express.js
+++ config/express.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 module.exports = function() {
     var app = express();
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
