0b46c0073cc498818bf2fdc3d4568a3b7d439a9b
Lucas Hanke <hankelucas@gmail.com>
2018-11-01 17:58:22 -0300

    #39 add user data encryption method and tests for it

--- app/services/encryptionService.js
+++ app/services/encryptionService.js
@@ -1,29 +1,28 @@
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 
-var kms = new AWS.KMS();
-var kmsKeyAlias = process.env.KMS_KEY_ALIAS;
+const kms = () => new AWS.KMS();
 
 module.exports.encrypt = async (data) => {
+  const kmsKeyAlias = process.env.KMS_KEY_ALIAS;
+  if (kmsKeyAlias === undefined) throw new Error("No KMS key inforned. Please set KMS_KEY_ALIAS env variable.");
+
   var params = {
     KeyId: kmsKeyAlias,
     Plaintext: data,
   };
   try {
-    result = await kms.encrypt(params).promise();
+    result = await kms().encrypt(params).promise();
     return Buffer.from(result.CiphertextBlob).toString("base64");
   } catch(err) {
-    throw new Error("Encrypting error");
+    throw new Error("Encryption error");
   }
-}
+};
 
-module.exports.decrypt = async (data) => {
-  var params = {
-    CiphertextBlob: Buffer.from(data, "base64"),
+module.exports.encryptUserData = async (report) => {
+  const { encrypt } = module.exports;
+  return {
+    ...report,
+    email: await encrypt(report.email),
+    name: report.name ? await encrypt(report.name) : report.name,
   };
-  try {
-    result = await kms.decrypt(params).promise();
-    return Buffer.from(result.Plaintext).toString();
-  } catch(err) {
-    throw new Error("Decrypting error");
-  }
-}
+};
---
+++ tests/services/encryptionService.test.js
@@ -0,0 +1,87 @@
+const encryptionService = require('../../app/services/encryptionService');
+const sinon = require('sinon');
+const AWS = require('aws-sdk');
+
+describe('encryptionService', () => {
+  describe('encrypt', async () => {
+    const { encrypt } = encryptionService;
+
+    let kmsStub = sinon.stub(AWS, 'KMS');
+
+    beforeEach(() => {
+      delete process.env.KMS_KEY_ALIAS;
+      process.env.KMS_KEY_ALIAS = 'alias/key';
+    });
+
+    afterEach(() => {
+      kmsStub.reset();
+    });
+
+    it('throws exception if KMS_KEY_ALIAS is not set', async () => {
+      delete process.env.KMS_KEY_ALIAS;
+
+      await expect(encrypt('teste')).rejects.toMatchObject({
+        message: 'No KMS key inforned. Please set KMS_KEY_ALIAS env variable.',
+      });
+    });
+
+    it('throws encryption error if kms encryption fails for any reason', async () => {
+      kmsStub.returns({
+        encrypt: () => ({
+          promise: () => Promise.reject('arbitrary kms error'),
+        })
+      });
+
+      await expect(encrypt('teste')).rejects.toMatchObject({
+        message: 'Encryption error',
+      });
+    });
+
+    it('returns encrypted value in base64 encoding', async () => {
+      kmsStub.returns({
+        encrypt: () => ({
+          promise: () => Promise.resolve({
+            CiphertextBlob: Buffer.from('encryptedAndEncodedValue@','base64'),
+          }),
+        })
+      });
+      expect(await encrypt('test')).toEqual('encryptedAndEncodedValue');
+    });
+  });
+
+  describe('encryptUserData', async () => {
+    const { encryptUserData } = encryptionService;
+
+    let encryptStub = sinon.stub(encryptionService, 'encrypt');
+
+    const report = {
+      email: 'marielle@elenao.com',
+      name: 'Marielle Franco',
+      gender: 'woman',
+    };
+
+    afterEach(() => {
+      encryptStub.reset();
+    });
+
+    it('returns report object with encrypted email and name', async () => {
+      encryptStub.withArgs('marielle@elenao.com').resolves('encryptedEmail');
+      encryptStub.withArgs('Marielle Franco').resolves('encryptedName');
+
+      const encryptedReport = await encryptUserData(report);
+
+      expect(encryptedReport.email).toEqual('encryptedEmail');
+      expect(encryptedReport.name).toEqual('encryptedName');
+    });
+
+    it('returns report object with empty name if the original is empty as well', async () => {
+      encryptStub.withArgs('marielle@elenao.com').resolves('encryptedEmail');
+      report.name = '';
+
+      const encryptedReport = await encryptUserData(report);
+
+      expect(encryptedReport.email).toEqual('encryptedEmail');
+      expect(encryptedReport.name).toEqual('');
+    });
+  });
+});
